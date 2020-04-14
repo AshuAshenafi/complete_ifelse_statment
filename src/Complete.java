67b1679245f67c1877ba725091cbb770a0b2967d
ashuashenafi <ashuashenafi@gmail.com>
2020-04-14 15:27:00 -0400

    first commit

---
+++ src/Complete.java
@@ -0,0 +1,15 @@
+public class Complete {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if(number > 10){
+            System.out.println("Number is greater than 10.");
+        }
+        else if(number < 10 ){
+            System.out.println("Number is less than 10.");
+        }
+        else {
+            System.out.println("Number is equal to 10.");
+        }
+    }
+}
